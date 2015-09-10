2ad2a1a343182b70832557c034334766bfe1ff38
Glenn Hurley <glenn.hurley@gmail.com>
2015-09-10 17:39:24 -0400

    Wrote required methods from step 1. 4 tests written to cover them.

--- movie_lib.py
+++ movie_lib.py
@@ -1,6 +1,8 @@
 import csv
 
 MOVIE_DICT = {}
+RATINGS = {}
+
 with open('movie_data.txt', 'r') as f:
     reader = csv.reader(f, delimiter='|')
     for row in reader:
@@ -9,6 +11,16 @@ with open('movie_data.txt', 'r') as f:
                               'Video_Date': row[3],
                               'IMDB': row[4],
                               'Genres': row[5:]}
+
+with open('data.txt') as datafile:
+    reader = csv.reader(datafile, delimiter=' ', skipinitialspace=True)
+    key_num = 1
+    for row in reader:
+        RATINGS[key_num] = {'user_id': row[0],
+                            'movie_id': row[1],
+                            'rating': row[2],
+                            'timestamp': row[3]}
+        key_num += 1
 
 
 class Movie:
@@ -20,12 +32,21 @@ class Movie:
         self.video_release_date = ''
         self.genres = []
 
-    def movie_by_id(inp):
+    def movie_by_id(self, inp):
         return MOVIE_DICT[inp]['Name']
+
+    def ratings_by_id(self, inp):
+        return [v['rating'] for k, v in RATINGS.items() if (v['movie_id']) == inp]
+
+    def avg_rating(self, inp):
+        ratings = [int(x) for x in self.ratings_by_id(inp)]
+        return "%.2f" % float(sum(ratings)/len(ratings))
 
 
 class User:
-    pass
+
+    def all_user_ratings(self, inp):
+        return [v['rating'] for k, v in RATINGS.items() if (v['user_id']) == inp]
 
 
 class Ratings:
