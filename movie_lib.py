7edde7aba1d85d6ab5ba68963f725ed0de9899e2
Glenn Hurley <glenn.hurley@gmail.com>
2015-09-11 14:26:40 -0400

    Created movie and user dictionary and add movie and user objectes to them when looping through the files.

--- movie_lib.py
+++ movie_lib.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Movie:
 
     def __init__(self, **kwargs):
@@ -18,12 +21,40 @@ class User:
     def __init__(self, **kwargs):
         self.id = ''
         self.name = ''
+        self.first_name = ''
+        self.middle_name = ''
+        self.last_name = ''
+        self.email = ''
         self.age = ''
         self.gender = ''
         self.occupation = ''
         self.zip = ''
         self.ratings = {}
-        self.avg_rating = {}
+        self.avg_rating = 0
 
         for k, v in kwargs.items():
             setattr(self, k, v)
+
+
+movie_dict = {}
+users = {}
+
+# Create Movie objects for each item in u.item file
+with open('u.item', encoding='latin_1') as f:
+    reader = csv.reader(f, delimiter='|')
+    for row in reader:
+        movie_dict[row[0]] = Movie(id=row[0],
+                                   name=row[1],
+                                   release_date=row[2],
+                                   video_release_date=row[3],
+                                   genres=row[4])
+
+# Create user objects for each item in u.user file
+with open('u.user') as f:
+    reader = csv.reader(f, delimiter='|')
+    for row in reader:
+        users[row[0]] = User(id=row[0],
+                             age=row[1],
+                             gender=row[2],
+                             occupation=row[3])
+
